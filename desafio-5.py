6be5fbf3b32d0068c72499b7fa69e7a6f83328c7
lbrasil73 <leandro almeida>
2026-01-30 18:17:39 -0300

    feature: add desafio 5

--- desafio-5.py
+++ desafio-5.py
@@ -1,16 +1,32 @@
-def check( enter ):
+def check(enter):
+
+    if not isinstance(enter, str):
+        raise TypeError(f"Entrada inválida: Esperava string, mas recebeu {type(enter).__name__}")
+    if not enter.strip():
+        raise ValueError("The paramenter cannot be empty.")
+
+    try:
+        price_open, price_close = map(int, enter.split())
+
+        if price_open > price_close:
+            return "ALTA"
+        elif price_close < price_open:
+            return "BAIXA"
+        else:
+            return "ESTAVEL"
+            
+    except ValueError:
+        return "Erro: A string deve conter dois números inteiros separados por espaço."
+
+
+try:
+    # print(check("10 15"))  
+    check("")
+    # print(check(1015))    
+except TypeError as e:
+    print(e)
     
-    values = enter.split(" ")
 
-    abertura = int( values[0] )
-    fechamento = int( values[1] )
 
-    if ( fechamento > abertura ):
-        print( "ALTA" )
-    elif ( fechamento < abertura):
-        print( "BAIXA")
-    else: 
-        print("ESTAVEL")
-        
 
 check( "10 15")
